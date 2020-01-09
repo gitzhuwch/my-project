110781e607c2d911f77db49ab8b79df6531cb728
wanchao <wczhu@iflytek.com>
2020-01-09 17:04:59 +0800

    fix ftrace config file

--- ftrace-config.sh
+++ ftrace-config.sh
@@ -10,14 +10,18 @@ if [ ! -e /bin/off ]; then
 	echo "echo 0 > /sys/kernel/debug/tracing/tracing_on" > /bin/off
 fi
 
-#cd /sys/kernel/debug/tracing 
-echo 0 > /sys/kernel/debug/tracing/tracing_on 
-echo function_graph > /sys/kernel/debug/tracing/current_tracer 
-echo nofuncgraph-irqs > /sys/kernel/debug/tracing/trace_options 
-echo funcgraph-proc > /sys/kernel/debug/tracing/trace_options 
-#echo $$ > /sys/kernel/debug/tracing/set_ftrace_pid 
-echo '' > /sys/kernel/debug/tracing/trace 
-#echo 1 > /sys/kernel/debug/tracing/tracing_on 
-cat /sys/kernel/debug/tracing/trace_pipe 
-#echo "=============================" > /sys/kernel/debug/tracing/trace_marker 
+#cd /sys/kernel/debug/tracing
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+echo function_graph > /sys/kernel/debug/tracing/current_tracer
+echo nofuncgraph-irqs > /sys/kernel/debug/tracing/trace_options
+echo funcgraph-proc > /sys/kernel/debug/tracing/trace_options
+echo nooverwrite > /sys/kernel/debug/tracing/trace_options
+#echo generic_handle_irq > /sys/kernel/debug/tracing/set_graph_function
+
+echo 50000 > /sys/kernel/debug/tracing/buffer_size_kb
+#echo $$ > /sys/kernel/debug/tracing/set_ftrace_pid
+echo '' > /sys/kernel/debug/tracing/trace
+#echo 1 > /sys/kernel/debug/tracing/tracing_on
+cat /sys/kernel/debug/tracing/trace_pipe
+#echo "=============================" > /sys/kernel/debug/tracing/trace_marker
 
