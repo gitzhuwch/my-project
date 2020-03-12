bdc59b66ee9ecd10fc730facf6bafba074e021af
wanchao <wczhu@iflytek.com>
2020-03-12 17:12:11 +0800

    1, git config difftool.prompt no 2, add qemu32 notes

--- new-ubuntu-init/2configs.sh
+++ new-ubuntu-init/2configs.sh
@@ -1,9 +1,12 @@
+#-------------------------------------------------------------
 #gnome-terminal config:
 #	font
 #	background color
 #	"reset and clear" shortcut
+#-------------------------------------------------------------
 #ubuntu desktop config:
 #	backgroud color
+#-------------------------------------------------------------
 #git config:
 #	git config --global alias.st status
 #	git config --global alias.br branch
@@ -11,28 +14,39 @@
 #	git config --global core.editor vim
 #	git config color.diff.whitespace "red reverse"
 #	git diff --ws-error-highlight=all
-#	git config diff.wsErrorHighlight all
+#	git config --global diff.wsErrorHighlight all
+#	git config --global difftool.prompt no
+#-------------------------------------------------------------
 #evince config:
 #	backgroud color
 #	asve as default config
+#-------------------------------------------------------------
 #firefox browser config:
 #	add-ons:search zoom->set default font size
 #	add-ons:search flash->
 #	add-ons:Dark Background and Light Text
+#-------------------------------------------------------------
 #vim-scripts config:
 #	cp -rf /usr/share/vim-scripts/ ~/.vim/
+#-------------------------------------------------------------
 #gnome-control-center config:
 #	----
+#-------------------------------------------------------------
 #gnome-tweaks config:
 #	----
+#-------------------------------------------------------------
 #update-manager config:
 #	----
+#-------------------------------------------------------------
 #nfs-kernel-server config:
 #	echo "/home/user/nfs *(rw,sync,no_root_squash)" > /etc/exports
+#-------------------------------------------------------------
 #everytime bootup show "System program problem detected" resolvent
 #	sudo vim /etc/default/apport modfy "enable=0"
+#-------------------------------------------------------------
 #windows(LTC) and ubuntu(UTC+8) double system time error
 #	modify windows regedit
+#-------------------------------------------------------------
 mkdir ~/work
 mkdir ~/bin
 mkdir ~/notes
