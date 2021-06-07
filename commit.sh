4c03503049fa293244dc904f88d02c3296625b98
zhuwch <zhuwch@starsmicrosystem.com>
2021-06-07 09:40:07 +0800

    fixed

--- commit.sh
+++ commit.sh
@@ -2,6 +2,10 @@
 git add .
 git commit -m "fixed"
 
-git push origin
+# git push origin
 
-while true; do git push github; if [ $? == 0 ]; then break; fi; done
+# while true; do git push github; if [ $? == 0 ]; then break; fi; done
+
+git push https://gitee.com/gitzhuwch/my-project master:master
+
+while true; do git push https://github.com/gitzhuwch/my-project master:master; if [ $? == 0 ]; then break; fi; done
