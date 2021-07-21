028cf71b22e068bfbe913a52da284f9236a3fcc5
zhuwch <zhuwch@starsmicrosystem.com>
2021-07-21 15:58:27 +0800

    commit.sh: add "git pull" after "git push"

--- commit.sh
+++ commit.sh
@@ -20,3 +20,5 @@ git push https://gitee.com/gitzhuwch/my-project master:master
 while true; do git push https://github.com/gitzhuwch/my-project master:master; if [ $? == 0 ]; then break; fi; done
 
 git branch -u origin/master
+
+git pull
