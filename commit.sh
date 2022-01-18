3c9f457712019b72fc89357ceb40606edcdec796
zhuwch <zhuwch@starsmicrosystem.com>
2022-01-18 11:15:43 +0800

    new-notes: add race and hazard and metastability.

--- commit.sh
+++ commit.sh
@@ -17,7 +17,8 @@ git commit -m "$message"
 
 git push https://gitzhuwch:oahcnawuhz123@gitee.com/gitzhuwch/my-project master:master
 
-while true; do git push https://gitzhuwch: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@github.com/gitzhuwch/my-project; if [ $? == 0 ]; then break; fi; done
+# latest token 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
+while true; do git push https://gitzhuwch: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@github.com/gitzhuwch/my-project; if [ $? == 0 ]; then break; fi; done
 
 git branch -u origin/master
 
